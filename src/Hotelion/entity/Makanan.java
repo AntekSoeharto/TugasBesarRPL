6465958f194a0ff0300dc94cf8ee266a9f4c73e8
AntekSoeharto <cocoalongs@gmail.com>
2021-03-26 11:55:51 +0700

    Membuat Class Makanan

---
+++ src/Hotelion/entity/Makanan.java
@@ -0,0 +1,51 @@
+package Hotelion.entity;
+
+public class Makanan {
+    private String id;
+    private String nama;
+    private double harga;
+    private int jumalah;
+
+    public Makanan(){
+
+    }
+
+    public Makanan(String id, String nama, double harga, int jumalah) {
+        this.id = id;
+        this.nama = nama;
+        this.harga = harga;
+        this.jumalah = jumalah;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
+    public int getJumalah() {
+        return jumalah;
+    }
+
+    public void setJumalah(int jumalah) {
+        this.jumalah = jumalah;
+    }
+}
